faf8c98f420a3ad1f283351356e6ad5ded32c6e1
Tim Riley <Titus-Riley@users.noreply.github.com>
2025-08-14 14:00:11 +0000

    Cleaned and added user modules

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,10 @@ def create_app(config_class='config.DevelopmentConfig'):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    # Initialize Extensions
     db.init_app(app)
+
+    # Register Blueprints
     app.register_blueprint(main_blueprint)
 
     return app
